ee90ceb8b183999f7796c495487b4803900487ba
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-07-09 11:47:51 +0000

    Trying to display index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+require( 'fs' );
+
+var indexFile;
+fs.readFile( 'index.html', function( err, data ) {
+    if ( err ) throw err;
+    indexFile = data;
+});
+var buffer = new Buffer( indexFile, 'utf-8' );
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send( buffer );
 });
 
 var port = process.env.PORT || 5000;
