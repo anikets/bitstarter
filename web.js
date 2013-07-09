652e80c3ff11bf3048370773246b34d19301e143
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-07-09 15:49:50 +0000

    Code clean up.

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
 var fs = require( 'fs' );
 
-var indexFile;
 var cont = fs.readFileSync( 'index.html', 'utf-8' );
-//var buffer = new Buffer( indexFile, 'utf8' );
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
     response.send( cont );
 });
 
