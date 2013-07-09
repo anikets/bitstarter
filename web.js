641dc1cb097c36236f3b39c565b530fffdbe30d1
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-07-09 12:48:27 +0000

    Debugging

--- web.js
+++ web.js
@@ -5,14 +5,14 @@ fs.readFile( 'index.html', function( err, data ) {
     if ( err ) throw err;
     indexFile = data;
 });
-var buffer = new Buffer( indexFile, 'utf-8' );
+var buffer = new Buffer( indexFile, 'utf8' );
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    response.send( buffer.toString() );
+    response.send( buffer.toString( 'utf8', 0, buffer.length );
 });
 
 var port = process.env.PORT || 5000;
