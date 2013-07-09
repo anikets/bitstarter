d60773e54bfc64e711252cab747a6f2c88a9e917
Aniket A. Suryavanshi <aniket.a.s@gmail.com>
2013-07-09 11:56:11 +0000

    Debugging

--- web.js
+++ web.js
@@ -11,8 +11,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-//    response.send( buffer );
+//  response.send('Hello World 2!');
+    response.send( indexFile );
 });
 
 var port = process.env.PORT || 5000;
